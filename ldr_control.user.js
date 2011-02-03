bceea323777d310ab0c14a75b26651d5f1894380
natsumesou <natsumesouxx@gmail.com>
2011-02-03 20:32:06 +0900

    checking eventlistener's behavior

--- ldr_control.user.js
+++ ldr_control.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        natsumesou
+// @name        ldr_control
 // @namespace   http://natsu.me
 // @description key control for livedoor reader
-// @include http://reader.livedoor.com/reader/
+// @include *
 // ==UserScript==
 
 var keybind = (function(){
@@ -15,6 +15,9 @@ var keybind = (function(){
         document.addEventListener("keydown",
             function(event){
             var tagName = event.target.tagName;
+
+            document.title=code(event);
+
             if(phrase == code(event) &&
                 !tagName.match(/(INPUT|TEXTAREA)/i)){
             func();
@@ -26,14 +29,17 @@ var keybind = (function(){
         }
         function code(event){
         var code = [];
+
         if(event.shiftKey){
             code.push("S");
-        } else if(event.ctrlKey){
+        }else if(event.ctrlKey){
             code.push("C");
-        } else if(event.altKey || event.metaKey){
+        }else if(event.altKey || event.metaKey){
             code.push("M");
         }
-        code.push(kc2char(event.keyCode));
+
+        var sub_keyCode =  kc2char(event.keyCode);
+        if(sub_keyCode) code.push(sub_keyCode);
         return code.join("-");
 
         function kc2char(kc){
@@ -42,11 +48,13 @@ var keybind = (function(){
             }
             var _32_40 = "space pageup pagedown end home left up right down".split(" ");
             var kt = {
-8  : "backspace",
+     8  : "backspace",
      9  : "tab"  ,
      13 : "enter",
      16 : "shift",
      17 : "control",
+     //16 : null,
+     //17 : null,
      27 : "escape",
      46 : "delete",
             };
@@ -62,5 +70,9 @@ var keybind = (function(){
 })();
 
 
-keybind("C-S", function(){ return false; });
+/*
+keybind("C-S", function(){ alert("hoge"); });
+*/
+
+keybind("C-a", function(){ document.title = "hoge"; });
 
