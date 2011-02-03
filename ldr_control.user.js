36111863d92795e12d30dcbad47502db4e0a214a
natsumesou <natsumesouxx@gmail.com>
2011-02-04 02:00:43 +0900

    fixed all of before code

--- ldr_control.user.js
+++ ldr_control.user.js
@@ -2,77 +2,42 @@
 // @name        ldr_control
 // @namespace   http://natsu.me
 // @description key control for livedoor reader
-// @include *
+// @include http://reader.livedoor.com/reader/*
 // ==UserScript==
 
-var keybind = (function(){
-        function add (phrase, func){
-        if(phrase instanceof Array){
-        phrase.forEach(function(p){
-            add(p, func);
-            });
-        } else {
-        document.addEventListener("keydown",
-            function(event){
-            var tagName = event.target.tagName;
+function evalInPage(fun) {
+      location.href = "javascript:void (" + fun + ")()";
+}
 
-            document.title=code(event);
+evalInPage(function () {
+    //無効にしたいキーバインドを登録するだけ。
+    Keybind.remove('shift+ctrl');
+    Keybind.remove('ctrl+shift');
+});
 
-            if(phrase == code(event) &&
-                !tagName.match(/(INPUT|TEXTAREA)/i)){
-            func();
-            event.preventDefault();
-            event.stopPropagation();
-            }
-            }, true);
-        }
-        }
-        function code(event){
-        var code = [];
-
-        if(event.shiftKey){
-            code.push("S");
-        }else if(event.ctrlKey){
-            code.push("C");
-        }else if(event.altKey || event.metaKey){
-            code.push("M");
-        }
-
-        var sub_keyCode =  kc2char(event.keyCode);
-        if(sub_keyCode) code.push(sub_keyCode);
-        return code.join("-");
-
-        function kc2char(kc){
-            function between(a,b){
-                return a <= kc && kc <= b;
-            }
-            var _32_40 = "space pageup pagedown end home left up right down".split(" ");
-            var kt = {
-     8  : "backspace",
-     9  : "tab"  ,
-     13 : "enter",
-     16 : "shift",
-     17 : "control",
-     //16 : null,
-     //17 : null,
-     27 : "escape",
-     46 : "delete",
-            };
-            return (between(65,90)  ? String.fromCharCode(kc+32) : // a-z
-                    between(48,57)  ? String.fromCharCode(kc) :    // 0-9
-                    between(96,105) ? String.fromCharCode(kc-48) : // num 0-9
-                    between(32,40)  ? _32_40[kc-32] :
-                    kt.hasOwnProperty(kc) ? kt[kc] :
-                    kc);
-        }
-        }
-        return add;
-})();
-
-
+// livedoor Reader KeyConfig
 /*
-keybind("C-S", function(){ alert("hoge"); });
+var KeyConfig = {
+     'read_next_subs'   : 's|shift+ctrl|shift+down',
+     'read_prev_subs'   : 'a|ctrl+shift|shift+up',
+     'read_head_subs'   : 'w|shift+home',
+     'read_end_subs'    : 'W|shift+end',
+     'feed_next'        : '>|J',
+     'feed_prev'        : '<|K',
+     'reload_subs'      : 'r',
+     'scroll_next_page' : 'space|pagedown',
+     'scroll_prev_page' : 'shift+space|pageup',
+     'pin'              : 'p',
+     'open_pin'         : 'o',
+     'view_original'    : 'v|ctrl+enter',
+     'scroll_next_item' : 'j|enter',
+     'scroll_prev_item' : 'k|shift+enter',
+     'compact'          : 'c',
+     'focus_findbox'    : 'f',
+     'blur_findbox'     : 'esc',
+     'unsubscribe'      : 'delete',
+     'toggle_leftpane'  : 'z',
+     'toggle_fullscreen': 'Z',
+     'toggle_keyhelp'   : '?'
+};
 */
-
-keybind("C-a", function(){ document.title = "hoge"; });
-
